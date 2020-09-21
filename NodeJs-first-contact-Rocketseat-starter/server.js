fd185fd02daa89eb76d0ac6afb19a554d21174ea
rjornelas <rafael.j.ornelas@gmail.com>
2020-09-21 12:23:56 -0300

    Adição do mongoose, require-dir e criação do model product

--- NodeJs-first-contact-Rocketseat-starter/server.js
+++ NodeJs-first-contact-Rocketseat-starter/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi'
+    , {useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+requireDir('./src/models');
+
+
+const Product = mongoose.model('Product');
 
 app.get('/', (req, res) => {
-    res.send('hello world!!!');
+    Product.create({
+        title: 'JavaScript',
+        description: 'First product created',
+        url: 'http://google.com',
+    });
+
+    return res.send("hello world");
 });
 
 app.listen(3001);
